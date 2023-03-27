267e6a3b268ba248978d2857352e7fb71851fc5a
sumbel <s.enikeeva1@mail.ru>
2023-03-27 21:15:04 +0300

    added perfs from model reader

--- reader.py
+++ reader.py
@@ -1,4 +1,6 @@
 import logging
+import re
+
 from dateutil import parser
 
 from helpers.utils import is_contains, to_dict
@@ -6,7 +8,52 @@ from services.read_service import read_df, processing_df
 
 
 def model_reader(path):
-    pass
+    def parse_date(part_):
+        date_str = part_[part_.find('\n') + 1:part_.find('/')]
+        return parser.parse(date_str)
+
+    def parse_intervals(part_):
+        int_idxs = [m.start() for m in re.finditer('COMPDATMD', part_)]
+        intervals_ = {}
+        if len(int_idxs) > 0:
+            int_idxs.append(len(part_))
+            for idx in range(len(int_idxs) - 1):
+                int_data = part_[int_idxs[idx]: int_idxs[idx + 1]].split('\n')
+                start = False
+                j = 0
+                while j < len(int_data):
+                    if int_data[j].strip().startswith('--'):
+                        start = True
+                        j += 1
+                        continue
+                    if start:
+                        if int_data[j].strip() == '/':
+                            break
+                        else:
+                            int_data_j = int_data[j].strip().split()[:6]
+                            if len(int_data_j) < 6:
+                                j += 1
+                                continue
+                            if intervals_.get(int_data_j[0][1:-1]) is None:
+                                intervals_[int_data_j[0][1:-1]] = int_data_j[1:]
+                            else:
+                                intervals_[int_data_j[0][1:-1]].append(int_data_j[1:])
+                    j += 1
+            return intervals_
+
+        else:
+            return []
+
+    with open(path, 'r') as f:
+        perf_data = f.read()
+    date_idxs = [m.start() for m in re.finditer('DATES', perf_data)] + [len(perf_data)]
+    model_perf = {}
+    for i in range(len(date_idxs) - 1):
+        part = perf_data[date_idxs[i]: date_idxs[i + 1]]
+        date = parse_date(part)
+        intervals = parse_intervals(part)
+        model_perf[date] = intervals
+    return model_perf
 
 
 def base_reader(path):
